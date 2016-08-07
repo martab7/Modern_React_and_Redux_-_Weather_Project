932355743d4d39a0a458b81f75b0b0724fc1280c
Anthony <martab7.am@gmail.com>
2016-08-07 15:37:38 -0500

    Adds integration of googlemaps for location when conducting search

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Chart from '../components/chart'
+import Chart from '../components/chart';
+import GoogleMap from '../components/googlemap';
 
 class WeatherList extends Component {
   renderWeather(cityData){
@@ -10,14 +11,14 @@ class WeatherList extends Component {
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp * 1.8 - 459.67) ;
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
-
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
-        <td><Chart data={temps} color="orange" units="F"/></td>
-        <td><Chart data={pressures} color="red" units="hpa"/></td>
-        <td><Chart data={humidities} color="black" units="%"/></td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="orange" units="F" /></td>
+        <td><Chart data={pressures} color="red" units="hpa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
       </tr>
     )
   }
